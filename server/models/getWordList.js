f62bb0a0501f2630d13827accdf897591cfac540
Stanisław Stankiewicz <60826828+stahuOfficial@users.noreply.github.com>
2024-07-08 10:01:00 +0200

    fix different line ending format case

--- server/models/getWordList.js
+++ server/models/getWordList.js
@@ -2,9 +2,13 @@ const fs = require("fs");
 
 const wordListPath = "resources/words.txt";
 
-const wordList = fs.readFileSync(wordListPath, "utf8").split("\r\n");
+const wordList = fs
+  .readFileSync(wordListPath, "utf8")
+  .replace(/\r\n/g, "\n")
+  .split("\n");
 
 const getWordList = (wordLength) => {
+  console.log(wordList[0].length);
   return wordList.filter((word) => word.length === wordLength);
 };
 
